e8703bff0db0a65e52c29a023c8a5696a536923e
Admin <Admin>
2019-10-09 19:22:06 +0530

    JS if-else, switch, while, for

---
+++ JS/02_loops.js
@@ -0,0 +1,41 @@
+
+// while loop
+/*
+//arithmetic progression
+let a = 2
+let d = 5
+let n = 6
+let v = a
+// tn = a + (n-1) * d
+
+while(v <= a + (n-1) * d){
+    console.log(v);
+    v = v + d
+}
+*/
+
+//for loops
+
+// for syntax
+// for(let i=1; i<=10; i=i+2){
+//     console.log(i);
+// }
+
+let my_arr = [12, 23, 65, 43, 22, 'text']
+// console.log(my_arr[5])
+
+
+for(let i=0; i < my_arr.length; i++){
+    console.log(my_arr[i])
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ JS/basics.js
@@ -0,0 +1,50 @@
+var a = 10;
+let b = 15;
+const c = 20;
+/*
+//if conditions
+
+if(a < 5){
+    console.log(`${a} is greater that 5`);
+} else if(a > 5){
+    console.log(`${a} is greater than 5`);
+} else if(a == 10){
+    console.log(`${a} is equal to 10`);
+} else{
+    console.log(`${a} value is not in this range.`)
+}
+*/
+
+/*
+//Switch case
+
+let dice = prompt('Enter your roll');
+switch(dice){
+    case '1':
+        console.log('move 1 step');
+        break;
+    
+    case '2':
+        console.log('move 2 step');
+        break;
+    
+    case '3':
+        console.log('move 3 step');
+        break;
+        
+    case '4':
+        console.log('move 4 step');
+        break;
+
+    case '5':
+        console.log('move 5 step');
+        break;
+    
+    case '6':
+        console.log('move 6 step');
+        break;
+    
+    default:
+        console.log('invalid roll');
+}
+*/
